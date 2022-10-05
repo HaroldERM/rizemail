18862c66b7858521dbe4fda1066b685df2d71fd9
Harold <61435652+HaroldERM@users.noreply.github.com>
2022-10-05 12:32:03 -0600

    ready to deploy

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,14 +6,13 @@ import { getStorage } from "firebase/storage";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBq9U2FzRosm31fTZ79FFIPaYh0vRfnryo",
-  authDomain: "rizemail-1f7ec.firebaseapp.com",
-  databaseURL: "https://rizemail-1f7ec-default-rtdb.firebaseio.com",
-  projectId: "rizemail-1f7ec",
-  storageBucket: "rizemail-1f7ec.appspot.com",
-  messagingSenderId: "986769287763",
-  appId: "1:986769287763:web:863c726de8c9ad45b6b4a6",
-  measurementId: "G-3Z050XGTGS"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
